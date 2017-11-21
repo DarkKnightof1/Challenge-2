3df448c8b9eaef952709985fe74ee45c3d78bb10
DarkKnightof1 <mischa@boskat.eu>
2017-11-21 18:56:43 +0100

    test

---
+++ JS1.js
@@ -0,0 +1,8 @@
+var getal = 10;
+for (var teller = 0; teller < 101.; teller++) {
+    console.log(getal + "*" + teller + "=" + (getal * teller));
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.font = "15px Arial";
+    ctx.fillText(getal + "*" + teller + "=" + (getal * teller), 10, 20 * teller);
+}
