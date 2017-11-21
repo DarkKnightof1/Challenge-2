4dbb952f07c08ca95e753342390eb2f911a5d93b
DarkKnightof1 <mischa@boskat.eu>
2017-11-21 20:04:07 +0100

    test

--- JS1.js
+++ JS1.js
@@ -1,8 +1,15 @@
-var getal = 10;
-for (var teller = 0; teller < 101.; teller++) {
-    console.log(getal + "*" + teller + "=" + (getal * teller));
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.font = "15px Arial";
-    ctx.fillText(getal + "*" + teller + "=" + (getal * teller), 10, 20 * teller);
+function myFunction() {
+    var getal = document.getElementById("myText").value;
+
+    for (var teller = 0; teller < 101.; teller++) {
+        console.log(getal + "*" + teller + "=" + (getal * teller));
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.font = "15px Arial";
+        ctx.fillText(getal + "*" + teller + "=" + (getal * teller), 10, 20 * teller);
+    }
+
+}
+function clear() {
+    location.reload();
 }
